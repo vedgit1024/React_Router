f248c081c2b8d4b2fc08fdefb17c450974299036
Kumar Vedant <kvedant1024@gmail.com>
2025-08-30 16:47:50 +0530

    Alternate(Older v6) Routing Definition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> },
-]);
+// const router = createBrowserRouter([
+//   { path: "/", element: <HomePage /> },
+//   { path: "/products", element: <ProductsPage /> },
+// ]);
+
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
+import { BrowserRouter, Routes, Route } from "react-router-dom"; //Older way of rounting - imp to know also
 
 function App() {
-  return <RouterProvider router={router} />;
+  //wrap our file with BrowserRouter
+  return (
+    <BrowserRouter basename="/app">
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+
+        <Route path="/products" element={<ProductsPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
